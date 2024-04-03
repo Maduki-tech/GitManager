d2a26a8a3a5544a8d12b8ba21f0717e2cea16820
Maduki-tech <d.schlueter1011@gmail.com>
2024-04-03 18:51:55 +0200

    added function and one main function to improve readablilty

--- manager.sh
+++ manager.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 isGitDir () {
     if [ -d .git ]; then
         return 0
@@ -8,24 +7,29 @@ isGitDir () {
     fi
 }
 
-
 branchIsDevelop () {
-    if [ $(git rev-parse --abbrev-ref HEAD) == "development" ]; then
+    if [ "$(git rev-parse --abbrev-ref HEAD)" == "development" ]; then
         return 0
     else
         return 1
     fi
 }
 
-for dir in *; do
-    if [ -d $dir ]; then
-        cd $dir
-        if isGitDir && branchIsDevelop; then
-            echo "Pulling $dir"
-            git pull
-        else
-            echo "Skipping $dir"
-        fi
-        cd ..
+handleGitPull () {
+    if isGitDir && branchIsDevelop; then
+        git pull
+    else
+        echo "$dir is not a git directory or branch is not development"
     fi
-done
+}
+
+main(){
+    for dir in *; do
+        if [ -d "$dir" ]; then
+            cd "$dir" || exit
+            handleGitPull;
+            cd ..
+        fi
+    done
+}
+main;
